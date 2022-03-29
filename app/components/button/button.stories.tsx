703d2cb05fc21143762f1cb361f3477f29742f5b
Alex Marshall <alex.k.marshall83@gmail.com>
2022-03-29 07:52:26 +0100

    refactor: improve story typescript

--- app/components/button/button.stories.tsx
+++ app/components/button/button.stories.tsx
@@ -1,7 +1,7 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react'
 import { ComponentProps, ReactNode, useLayoutEffect } from 'react'
 
 import { Button } from '.'
-import { ComponentMeta } from '@storybook/react'
 
 const meta: ComponentMeta<typeof Button> = {
   title: 'Components/Button',
@@ -11,53 +11,50 @@ const meta: ComponentMeta<typeof Button> = {
 
 export default meta
 
-type ButtonStory = {
-  args: ComponentProps<typeof Button>
-}
-
-const defaultArgs: ButtonStory['args'] = {
+// type ButtonStory = ComponentStory<typeof Button>
+const defaultArgs: ComponentProps<typeof Button> = {
   label: 'Press Me',
 }
 
-export const Primary: ButtonStory = {
-  args: {
-    ...defaultArgs,
-    color: 'primary',
-  },
+const Template: ComponentStory<typeof Button> = (args) => (
+  <Button {...defaultArgs} {...args} />
+)
+
+export const Primary = Template.bind({})
+
+export const Secondary = Template.bind({})
+Secondary.args = {
+  color: 'secondary',
 }
 
-export const Secondary: ButtonStory = {
-  args: {
-    ...defaultArgs,
-    color: 'secondary',
-  },
+export const SecondaryDarkMode = Template.bind({})
+SecondaryDarkMode.args = {
+  ...Secondary.args,
 }
+SecondaryDarkMode.decorators = [
+  (Story) => (
+    <DarkMode>
+      <Story />
+    </DarkMode>
+  ),
+]
 
-export const SecondaryDark = {
-  args: {
-    ...Secondary.args,
-  },
-  decorators: [
-    (Story) => (
-      <DarkMode>
-        <Story />
-      </DarkMode>
-    ),
-  ],
-}
+export const Mono = Template.bind({})
+Mono.args = { color: 'mono' }
 
-export const Mono: ButtonStory = {
-  args: {
-    ...defaultArgs,
-    color: 'mono',
-  },
-}
+export const MonoDarkMode = Template.bind({})
+MonoDarkMode.args = { ...Mono.args }
+MonoDarkMode.decorators = [
+  (Story) => (
+    <DarkMode>
+      <Story />
+    </DarkMode>
+  ),
+]
 
-export const Danger: ButtonStory = {
-  args: {
-    ...defaultArgs,
-    color: 'danger',
-  },
+export const Danger = Template.bind({})
+Danger.args = {
+  color: 'danger',
 }
 
 function DarkMode({ children }: { children: ReactNode }) {
