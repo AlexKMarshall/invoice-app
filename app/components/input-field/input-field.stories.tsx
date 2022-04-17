512d516aa8e7cde97c4bc7dab8d93a9778d8aee1
Alex Marshall <alex.k.marshall83@gmail.com>
2022-04-17 13:57:57 +0100

    test: input field

--- app/components/input-field/input-field.stories.tsx
+++ app/components/input-field/input-field.stories.tsx
@@ -1,7 +1,9 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react'
+import { userEvent, within } from '@storybook/testing-library'
 
 import { InputField } from '.'
 import { darkMode } from '~/storybook-helpers/dark-mode'
+import { expect } from '@storybook/jest'
 
 const meta: ComponentMeta<typeof InputField> = {
   title: 'Components/InputField',
@@ -21,6 +23,15 @@ const Template: ComponentStory<typeof InputField> = (args) => (
 )
 
 export const Default = Template.bind({})
+Default.play = async ({ args, canvasElement }) => {
+  const canvas = within(canvasElement)
+  const input = await canvas.findByRole('textbox', { name: args.label })
+  await userEvent.clear(input)
+  await userEvent.type(input, 'New address value')
+
+  await expect(input).toHaveValue('New address value')
+  await expect(input).toBeValid()
+}
 
 export const DefaultDarkMode = Template.bind({})
 DefaultDarkMode.decorators = [darkMode]
@@ -28,6 +39,13 @@ DefaultDarkMode.decorators = [darkMode]
 export const WithError = Template.bind({})
 WithError.args = {
   errorMessage: `Can't be empty`,
+}
+WithError.play = async ({ args, canvasElement }) => {
+  const canvas = within(canvasElement)
+  const input = await canvas.findByRole('textbox', { name: args.label })
+
+  await expect(input).toBeInvalid()
+  await expect(input).toHaveErrorMessage(args.errorMessage)
 }
 
 export const WithErrorDarkMode = Template.bind({})
