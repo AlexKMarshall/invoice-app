883769c149c84c663858ce7525c5042127a6ca46
Alex Marshall <alex.k.marshall83@gmail.com>
2022-05-23 10:42:56 +0100

    refactor: extract button utility classes

--- app/components/button/button.tsx
+++ app/components/button/button.tsx
@@ -17,13 +17,19 @@ export function Button({
   color = 'primary',
   ...props
 }: Props): JSX.Element {
-  const utilityClasses = ['radius-pill']
   const blockClass = 'button'
   const variantClass = colorClassMap[color]
 
-  const className = clsx(utilityClasses, blockClass, variantClass)
   return (
-    <button className={className} {...props}>
+    <button
+      className={clsx(
+        'rounded-full py-4 px-6 font-bold outline outline-offset-0 outline-transparent',
+        'focus-visible:outline-2 focus-visible:outline-offset-4',
+        blockClass,
+        variantClass
+      )}
+      {...props}
+    >
       {label}
     </button>
   )
