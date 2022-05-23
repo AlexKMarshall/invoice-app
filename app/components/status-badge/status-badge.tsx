ef1b196b105db56f3eb87c78c773ae44ae6d061f
Alex Marshall <alex.k.marshall83@gmail.com>
2022-05-23 07:00:19 +0100

    style: extract status-badge utility styles

--- app/components/status-badge/status-badge.tsx
+++ app/components/status-badge/status-badge.tsx
@@ -10,10 +10,18 @@ type Props = {
   status: 'pending' | 'paid' | 'draft'
 }
 export function StatusBadge({ status }: Props): JSX.Element {
-  const utilityClasses = ['radius-s']
-  const blockClass = 'status-badge'
   const variantClass = statusClassMap[status]
-  const className = clsx(utilityClasses, blockClass, variantClass)
 
-  return <p className={className}>{status}</p>
+  return (
+    <p
+      className={clsx(
+        'inline-flex items-baseline justify-center gap-2 rounded px-4 py-3 font-bold capitalize',
+        'before:aspect-square before:w-2 before:rounded-full', // bullet icon
+        'status-badge',
+        variantClass
+      )}
+    >
+      {status}
+    </p>
+  )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,29 @@
 module.exports = {
-  content: ["./app/**/*.{ts,tsx,jsx,js}"],
+  content: ['./app/**/*.{ts,tsx,jsx,js}'],
   theme: {
+    borderRadius: {
+      none: 0,
+      DEFAULT: '0.25rem',
+      md: '0.375rem',
+      lg: '0.5rem',
+      full: '100rem',
+      circle: '50%',
+    },
+    spacing: {
+      1: '0.375rem',
+      2: '0.5rem',
+      3: '0.75rem',
+      4: '1rem',
+      5: '1.25rem',
+      6: '1.5rem',
+      7: '2rem',
+      8: '2.5rem',
+      9: '3rem',
+      10: '3.5rem',
+      11: '4rem',
+      12: '4.5rem',
+    },
     extend: {},
   },
   plugins: [],
-};
+}
